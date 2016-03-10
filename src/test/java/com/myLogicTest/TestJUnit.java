2b8572c5a6b2eb0dfb9c9f6b15cf590a9d88ace1
bobrutskovav <thirdstrike@yandex.ru>
2016-03-10 22:29:19 +0300

    Still dont works...

--- src/test/java/com/myLogicTest/TestJUnit.java
+++ src/test/java/com/myLogicTest/TestJUnit.java
@@ -2,7 +2,6 @@ package com.myLogicTest;
 
 import com.myLogic.Calculator;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -26,9 +25,9 @@ public class TestJUnit {
     public String operation;
     public int expectedResult;
 
-
-
     public static Object[][] data;
+
+
 
     public TestJUnit(int firstParameter, int secondParameter, String operation, int expectedResult) {
         this.firstParameter = firstParameter;
@@ -38,24 +37,8 @@ public class TestJUnit {
 
     }
 
-        @BeforeClass
-        public static void makeData() throws IOException {
+       // @BeforeClass
 
-            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\third\\IdeaProjects\\MyJunit\\src\\test\\java\\com\\myLogicTest\\datafile.csv"));
-
-            ArrayList<Object[]> tempArray = new ArrayList<Object[]>();
-            String newLine;
-            Object[] oneString;
-            while ((newLine = reader.readLine()) != null) {
-
-
-                oneString = newLine.split(";");
-                tempArray.add(oneString);
-            }
-            data = new Object[tempArray.size()][];
-            for (int i = 0; i < data.length; i++) {
-                Object[] row = tempArray.get(i); data[i] = row; }
-            }
 
 
 
@@ -69,22 +52,24 @@ public class TestJUnit {
     public void checkCalculator() {
             final Calculator calculator = new Calculator(firstParameter, secondParameter, operation);
             int result;
-            if (operation.equals("*")) {
-                result = calculator.multi();
-                Assert.assertTrue("Результат (" + result + ") не равен" + expectedResult, result == expectedResult);
-            }
-            else if (operation.equals("+")) {
-                result = calculator.plus();
-                Assert.assertTrue("Результат (" + result + ") не равен" + expectedResult, result == expectedResult);
-            }
-            else if (operation.equals("-")) {
-                result = calculator.minus();
-                Assert.assertTrue("Результат (" + result + ") не равен" + expectedResult, result == expectedResult);
-            }
-            else if (operation.equals("/")) {
-                result = calculator.del();
+            switch (operation) {
+                case "*":
+                    result = calculator.multi();
+                    Assert.assertTrue("Результат (" + result + ") не равен" + expectedResult, result == expectedResult);
+                    break;
+                case "+":
+                    result = calculator.plus();
+                    Assert.assertTrue("Результат (" + result + ") не равен" + expectedResult, result == expectedResult);
+                    break;
+                case "-":
+                    result = calculator.minus();
+                    Assert.assertTrue("Результат (" + result + ") не равен" + expectedResult, result == expectedResult);
+                    break;
+                case "/":
+                    result = calculator.del();
 
-                Assert.assertTrue("Результат (" + result + ") не равен" + expectedResult, result == expectedResult);
+                    Assert.assertTrue("Результат (" + result + ") не равен" + expectedResult, result == expectedResult);
+                    break;
             }
 
             }
@@ -92,12 +77,37 @@ public class TestJUnit {
 
         @Parameterized.Parameters(name = "{index}: Действие {0} {2} {1} = {3}")
         public static Collection<Object[]> getTestData() {
-            return Arrays.asList(data
-                    /* new Object[][]{
+
+            try {
+                makeData();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return Arrays.asList(data);
+        }
+
+    /* new Object[][]{
                     {2, 2, "*", 4},
                     {2, 0, "+" , 2},
                     {2, 2,"/", 1},
                     {0, 2,"-",-2}
-            }*/);
+            }*/
+
+    public static void makeData() throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\third\\IdeaProjects\\MyJunit\\src\\test\\java\\com\\myLogicTest\\datafile.csv"));
+
+        ArrayList<Object[]> tempArray = new ArrayList<>();
+        String newLine;
+        Object[] oneString;
+        while ((newLine = reader.readLine()) != null) {
+
+
+            oneString = newLine.split(";");
+            tempArray.add(oneString);
         }
+        data = new Object[tempArray.size()][];
+        for (int i = 0; i < data.length; i++) {
+            Object[] row = tempArray.get(i); data[i] = row; }
+    }
 }
